159706fb1d31405f201f1095a6196f2a7a345dc9
Szantai Gabor <szantai.gabor.19f@szlgbp.hu>
2022-02-08 10:43:34 +0100

    Ready

--- 33-40.py
+++ 33-40.py
@@ -1,16 +1,5 @@
 from main import Telepules
 
-"""
-33)	Írja ki a Makói kistérség településeinek egy lakásra jutó népesség számát!
-34)	Írja ki a Szentesi kistérség településeinek egy lakásra jutó népesség számát!
-35)	Írja ki a Szegedi kistérség településeinek egy lakásra jutó népesség számát!
-36)	Írja ki a Kisteleki kistérség településeinek egy lakásra jutó népesség számát!
-37)	Melyik településen a legrosszabb a lakáshelyzet? (Egy lakásra a legtöbb lakos jut.)
-38)	Készítsen kimutatást kistérségi bontásban, amelyben megadja az egyes kistérségek településeinek a számát!
-39)	Készítsen kimutatást kistérségi bontásban, amelyben megadja az egyes kistérségek összlakosságának a számát!
-40)	Készítsen kimutatást kistérségi bontásban, amelyben megadja az egyes kistérségek összterületének a nagyságát!
-"""
-
 #33-36
 
 def térségek_lakására_jutók(lista, bekistérség):
@@ -20,21 +9,59 @@ def térségek_lakására_jutók(lista, bekistérség):
         if elem.kister_bes==bekistérség:
             népesség_ered+=elem.nepesseg
             lakások+=elem.lakossag
-    print (népesség_ered, lakások)
     népesség_ered/=lakások
     print (népesség_ered)
+print("33)	Írja ki a Makói kistérség településeinek egy lakásra jutó népesség számát!")
 print(térségek_lakására_jutók(Telepules.lista,"Makói"))
-#37
 
-#38-40
+print("34)	Írja ki a Szentesi kistérség településeinek egy lakásra jutó népesség számát!")
+print(térségek_lakására_jutók(Telepules.lista,"Szentesi"))
 
-def kimutatások(lista, adat):
-    első=lista[0].kister_bes
+print("35)	Írja ki a Szegedi kistérség településeinek egy lakásra jutó népesség számát!")
+print(térségek_lakására_jutók(Telepules.lista,"Szegedi"))
+
+print("36)	Írja ki a Kisteleki kistérség településeinek egy lakásra jutó népesség számát!")
+print(térségek_lakására_jutók(Telepules.lista,"Kisteleki"))
+
+#38
+def kisterseg_telepules(lista):
     kisterseg={}
-    i =1
+    i =0
     while i<len(lista):
+        if not lista[i].kister_bes in kisterseg.keys():
+            kisterseg[lista[i].kister_bes] =1
+        else:
+            kisterseg[lista[i].kister_bes] +=1
+        i+=1  
+    print(kisterseg)
+print("38)	Készítsen kimutatást kistérségi bontásban, amelyben megadja az egyes kistérségek településeinek a számát!")
+print(kisterseg_telepules(Telepules.lista))
 
+#39
+def kisterseg_lakosok(lista):
+    kisterseg={}
+    i =0
+    while i<len(lista):
+        if not lista[i].kister_bes in kisterseg.keys():
+            kisterseg[lista[i].kister_bes] =lista[i].nepesseg
+        else:
+            kisterseg[lista[i].kister_bes] +=lista[i].nepesseg
+        i+=1  
+    print(kisterseg) 
+print("39)	Készítsen kimutatást kistérségi bontásban, amelyben megadja az egyes kistérségek összlakosságának a számát!")
+print(kisterseg_lakosok(Telepules.lista))
+
+#40
+def kisterseg_terület(lista):
+    kisterseg={}
+    i =0
+    while i<len(lista):
+        if not lista[i].kister_bes in kisterseg.keys():
+            kisterseg[lista[i].kister_bes] =lista[i].terulet
+        else:
+            kisterseg[lista[i].kister_bes] +=lista[i].terulet
         i+=1   
-        
-
+    print(kisterseg)
+print("40)	Készítsen kimutatást kistérségi bontásban, amelyben megadja az egyes kistérségek összterületének a nagyságát!")
+print(kisterseg_terület(Telepules.lista))
 
